679aabb0b9ee8803108e35a92cbfcfdbd1feb630
Ryan Hafener <contact@rhafener.com>
2022-04-04 20:59:32 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,5 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hi There! I am Ryan, a current student who is interested in cybersecurity, political science, and politics.<div>
-}
-export default function Home() {
-  return 
- <div>More is coming soon! In the meantime, feel free to contact me at ryan (at) rhafener (dot) com.</div>
+  return <div> Hi There! I am Ryan, a current student who is interested in cybersecurity, political science, and politics. <div>
 }
